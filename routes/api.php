6758e56cb6e132b3eca220a799ede98ef3394950
chiba008007 <chiba00807@gmail.com>
2024-10-10 00:54:32 +0900

    api確認

--- routes/api.php
+++ routes/api.php
@@ -34,4 +34,4 @@ Route::post('mail', [MailController::class, 'send']);
 Route::post("login", [UserController::class, 'index']);
 Route::post("upload", [UserController::class, 'upload']);
 Route::get("top", [UserController::class, 'top']);
-Route::get("status", [UserController::class, 'status']);
+Route::apiResource("status", [UserController::class, 'status']);
